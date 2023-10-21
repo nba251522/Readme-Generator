2f5c882e7e3b64fa5aa986b2dfe66b8c9b13f1d4
nba251522 <inquister112345678@gmail.com>
2023-10-20 22:57:15 -0700

    added license link function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -9,7 +9,22 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+      return '';
+  }
+  switch(license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPL 3.0':
+      return 'https://opensource.org/licenses/GPL-3.0';
+    // Add more licenses as needed.
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
